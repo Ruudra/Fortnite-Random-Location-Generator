6dc0334e7b14cadebc651abf4758bf0113d20b39
Ruudra <37314102+Ruudra@users.noreply.github.com>
2018-04-15 15:49:06 -0400

    Add files via upload

---
+++ ftn_map_location_generator.py
@@ -0,0 +1,7 @@
+import random 
+
+place = random.choice(["Anarchy Acres", "Dusty Depot", "Fatal Fields", "Flush Factory", "Greasy Grove", "Haunted Hills", "Junk Junction", 
+				"Lonely Lodge", "Loot Lake", "Lucky Landing", "Moisty Mire", "Pleasant Park", "Retail Row", "Salty Springs", "Shifty Shafts",
+				"Snobby Shores", "Tilted Towers", "Tomato Town", "Wailing Woods"])
+				
+print(place)
